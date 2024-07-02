0acb44e507c9f7cf49326fe56dbd497de3d87045
brown5lc <brown5lc@mail.uc.edu>
2024-07-02 17:47:31 -0400

    Added a very basic function to read the frames of the video from the local 'videos' directory.

--- main.py
+++ main.py
@@ -4,3 +4,21 @@ import numpy as np
 def main():
 
     return
+
+def capture_video(path):
+
+    cap = cv.VideoCapture(path)
+
+    if(not(cap.isOpenend())):
+        return
+
+    while True:
+        isTrue, frame = cap.read()
+        cv.imshow('Video', frame)
+
+        if(cv.waitKey(20) & 0xFF == ord('d')):
+           break
+
+    cap.release()
+    cv.destroyAllWindows()
+    cv.waitKey(0)
